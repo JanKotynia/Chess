20fc5db188d641e87038210cf38bafe6eaa01c91
JanKotynia <jasiek.kotynia@gmail.com>
2024-08-06 09:43:48 +0200

    bishop img fix

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,4 +1,7 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Bishop extends Figure{
     Bishop(boolean s,int x,int y)
@@ -6,8 +9,17 @@ public class Bishop extends Figure{
         posx=x;
         posy=y;
         side=s;
-        if(s)
-            image = new ImageIcon("C:\\Users\\jasie\\IdeaProjects\\Git-test\\Chess\\src\\chess_f\\rojfel_w.png").getImage();
+        if(s) {
+            InputStream imageStream = Main.class.getResourceAsStream("/chess_f/rojfel_b.png");
+            if (imageStream == null) {
+                System.err.println("Nie znaleziono pliku: " + "/chess_f/rojfel_b.png");
+            }
+            try {
+                image = ImageIO.read(imageStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         else
             image = new ImageIcon("C:\\Users\\jasie\\IdeaProjects\\Git-test\\Chess\\src\\chess_f\\rojfel_b.png").getImage();
 
